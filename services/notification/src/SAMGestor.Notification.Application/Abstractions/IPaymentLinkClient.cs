a039fb9eecac799032438b197639fe4ebac9d28a
Lucas Magalhães <lucasmagalhaes728@gmail.com>
2025-08-31 18:54:55 -0300

    refactor(notification-payment): removendo IPaymentLinkClient/FakePaymentLinkClient e NotificationOrchestrator agora fluxo de serviço de pagamento da api

--- services/notification/src/SAMGestor.Notification.Application/Abstractions/IPaymentLinkClient.cs
+++
@@ -1,6 +0,0 @@
-namespace SAMGestor.Notification.Application.Abstractions;
-
-public interface IPaymentLinkClient
-{
-    Task<string> CreatePaymentLinkAsync(Guid registrationId, CancellationToken ct);
-}
