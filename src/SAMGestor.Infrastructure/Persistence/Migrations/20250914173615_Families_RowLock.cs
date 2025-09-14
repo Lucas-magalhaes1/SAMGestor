fa3a0829d15a3845cf61e4d2c55a34a702c50144
Lucas Magalhães <lucasmagalhaes728@gmail.com>
2025-09-14 17:32:04 -0300

    feat(migrations): Families_RowLock e atualizar SAMContextModelSnapshot

---
+++ src/SAMGestor.Infrastructure/Persistence/Migrations/20250914173615_Families_RowLock.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SAMGestor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Families_RowLock : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_locked",
+                schema: "core",
+                table: "families",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_locked",
+                schema: "core",
+                table: "families");
+        }
+    }
+}
