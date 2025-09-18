af5db2f28e79a207007104bb38239adb2d3a9203
Lucas Magalhães <lucasmagalhaes728@gmail.com>
2025-09-18 11:18:42 -0300

    test(builders): adicionar TestBuilders para facilitar arranjos de famílias/inscrições

---
+++ tests/SAMGestor.UnitTests/Builders/TestBuilders.cs
@@ -0,0 +1,28 @@
+using SAMGestor.Domain.Entities;
+using SAMGestor.Domain.Enums;
+using SAMGestor.Domain.ValueObjects;
+
+public static class TestBuilders
+{
+    public static Retreat NewOpenRetreat()
+        => new Retreat(
+            new FullName("Retiro X"), "ED1", "Tema",
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(10)),
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(12)),
+            100, 100,
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(-1)),
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(5)),
+            new Money(0,"BRL"), new Money(0,"BRL"),
+            new Percentage(50), new Percentage(50));
+
+    public static Registration NewReg(Guid retreatId, string name, Gender g, RegistrationStatus st = RegistrationStatus.Confirmed)
+        => new Registration(
+            new FullName(name), new CPF("52998224725"), new EmailAddress($"{Guid.NewGuid()}@mail.com"),
+            "11999999999", new DateOnly(1990,1,1), g, "SP", st, ParticipationCategory.Guest, "Oeste", retreatId);
+
+    public static Family NewFamily(Guid retreatId, string name = "Família 1", int capacity = 4)
+        => new Family(new FamilyName(name), retreatId, capacity);
+
+    public static FamilyMember Link(Guid retreatId, Guid familyId, Guid regId, int pos)
+        => new FamilyMember(retreatId, familyId, regId, pos);
+}
